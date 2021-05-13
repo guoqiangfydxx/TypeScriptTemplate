43dc9d6e1f7c3e272df91dc06d41b7ab493ab4b1
解锋镝 <guoqiang2576@gmail.com>
2021-05-13 18:08:38 +0800

    react-redux

--- src/React/redux/mockReactRedux.js
+++ src/React/redux/mockReactRedux.js
@@ -13,6 +13,7 @@ export class Provider extends React.Component {
 
   render() {
     const { store } = this.props;
+    // Provider利用了Context.Provider来实现，但是实际的代码中除了把store作为props传递下去之外，还有一个subscription对象
     return (
       <ReduxContext.Provider value={store}>
         {this.props.children}
@@ -27,6 +28,7 @@ export class Provider extends React.Component {
 /**
  * @param {JSX.IntrinsicAttributes} WrppedComponent
  */
+// 现在的connect的内部逻辑已经全都走的是connectAdvanced里面的逻辑
 export const connect = (mapStateToProps, mapDispatchToProps) => (
   WrppedComponent
 ) => {
@@ -49,6 +51,8 @@ export const connect = (mapStateToProps, mapDispatchToProps) => (
       });
     }
 
+    // 如果要是不参照现在的源码中做的东西的话，那么从简单意义上去理解，mapStateToProps和mapDispatchToProps这两个函数生成的对象都会被作为一部分prop合并到props中传递给当前组件
+    // 分别执行这两个函数然后保存下来，最后传递给包裹组件，这样理解起来是最容易的
     update = () => {
       const { getState, dispatch } = this.context;
       const state = getState();
